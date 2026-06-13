1bb7ba56b112aee3e2ecb761ef247c0f1aee9dec
Your Name <your.email@example.com>
2026-06-13 08:00:46 +0300

    chore: remove tracked temporary artifacts

--- patch2.rs
+++
@@ -1,3 +0,0 @@
-        nr::ZIQA_CAP_REQUEST => return ziqa_cap_request(ctx),
-        nr::ZIQA_CAP_READ    => return ziqa_cap_read(ctx),
-        nr::ZIQA_CAP_WRITE   => return ziqa_cap_write(ctx),
